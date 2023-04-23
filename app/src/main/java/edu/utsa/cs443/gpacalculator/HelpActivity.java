02408d13e050dad717893ea571406518a0d3a1a9
Esteban Leal <106554825+estebanleal1315@users.noreply.github.com>
2023-04-23 18:27:10 -0500

    Settings and help screens updates

---
+++ app/src/main/java/edu/utsa/cs443/gpacalculator/HelpActivity.java
@@ -0,0 +1,16 @@
+package edu.utsa.cs443.gpacalculator;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import edu.utsa.cs443.gpacalculator.R;
+
+public class HelpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.help_activity);
+    }
+}
--- app/src/main/java/edu/utsa/cs443/gpacalculator/controller/SettingsController.java
+++ app/src/main/java/edu/utsa/cs443/gpacalculator/controller/SettingsController.java
@@ -3,7 +3,6 @@ package edu.utsa.cs443.gpacalculator.controller;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Switch;
@@ -12,10 +11,12 @@ import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 
 import edu.utsa.cs443.gpacalculator.R;
+import edu.utsa.cs443.gpacalculator.HelpActivity;
 
 public class SettingsController implements View.OnClickListener {
 
     private final AppCompatActivity activity;
+    private final Switch themeSwitch;
 
     public SettingsController(AppCompatActivity activity) {
         this.activity = activity;
@@ -23,11 +24,19 @@ public class SettingsController implements View.OnClickListener {
         // Set up the click listeners for the buttons and switches
         Button aboutButton = activity.findViewById(R.id.about_button);
         Button helpButton = activity.findViewById(R.id.help_button);
-        Switch themeSwitch = activity.findViewById(R.id.theme_switch);
+        themeSwitch = activity.findViewById(R.id.theme_switch);
 
         aboutButton.setOnClickListener(this);
         helpButton.setOnClickListener(this);
         themeSwitch.setOnClickListener(this);
+
+        // Set the initial state of the theme switch
+        SharedPreferences preferences = activity.getPreferences(Context.MODE_PRIVATE);
+        boolean isDarkTheme = preferences.getBoolean("isDarkTheme", false);
+        themeSwitch.setChecked(isDarkTheme);
+        if (isDarkTheme) {
+            activity.setTheme(R.style.DarkTheme_GpaCalculator);
+        }
     }
 
     @Override
@@ -37,7 +46,7 @@ public class SettingsController implements View.OnClickListener {
                 showAbout();
                 break;
             case R.id.help_button:
-                showHelp();
+                showHelpScreen();
                 break;
             case R.id.theme_switch:
                 toggleTheme();
@@ -52,10 +61,9 @@ public class SettingsController implements View.OnClickListener {
         Toast.makeText(activity, message, Toast.LENGTH_SHORT).show();
     }
 
-    private void showHelp() {
-        // Display the "help" message
-        String message = "To use this app, enter your grades and credit hours for each course, then click the Calculate GPA button";
-        Toast.makeText(activity, message, Toast.LENGTH_SHORT).show();
+    public void showHelpScreen() {
+        Intent intent = new Intent(activity, HelpActivity.class);
+        activity.startActivity(intent);
     }
 
     private void toggleTheme() {
@@ -70,9 +78,9 @@ public class SettingsController implements View.OnClickListener {
 
         // Apply the new theme
         if (!isDarkTheme) {
-            activity.setTheme(R.style.Theme_GpaCalculator);
-        } else {
             activity.setTheme(R.style.DarkTheme_GpaCalculator);
+        } else {
+            activity.setTheme(R.style.Theme_GpaCalculator);
         }
 
         // Restart the activity to apply the new theme
