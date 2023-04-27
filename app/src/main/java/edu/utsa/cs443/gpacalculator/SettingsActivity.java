d89dbe4c763afc95a27b1c7aa2f9588cfb58aaa5
Esteban Leal <106554825+estebanleal1315@users.noreply.github.com>
2023-04-27 18:05:23 -0500

    Settings and help screens updates

--- app/src/main/java/edu/utsa/cs443/gpacalculator/SettingsActivity.java
+++ app/src/main/java/edu/utsa/cs443/gpacalculator/SettingsActivity.java
@@ -3,16 +3,64 @@ package edu.utsa.cs443.gpacalculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.Switch;
+import android.widget.Toast;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
 
 import edu.utsa.cs443.gpacalculator.R;
 import edu.utsa.cs443.gpacalculator.controller.SettingsController;
 
 public class SettingsActivity extends AppCompatActivity {
 
+    private SettingsController settingsController;
+    private Switch themeSwitch;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.settings_activity);
-        new SettingsController(this);
+
+        settingsController = new SettingsController(this);
+
+        // Find the Save button and set a click listener to save the settings to a file
+        Button saveButton = findViewById(R.id.save_button);
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                saveSettingsToFile();
+            }
+        });
+    }
+
+    private void saveSettingsToFile() {
+        // Get the current state of the notification checkbox and theme switch
+        boolean notificationsEnabled = ((CheckBox) findViewById(R.id.notification_checkbox)).isChecked();
+        boolean isDarkTheme = ((Switch) findViewById(R.id.theme_switch)).isChecked();
+
+        // Write the settings to a file
+        try {
+            FileOutputStream fileOutputStream = openFileOutput("settings.txt", MODE_PRIVATE);
+            OutputStreamWriter outputStreamWriter = new OutputStreamWriter(fileOutputStream);
+            outputStreamWriter.write(notificationsEnabled + "," + isDarkTheme);
+            outputStreamWriter.close();
+            Toast.makeText(this, "Settings saved", Toast.LENGTH_SHORT).show();
+        } catch (IOException e) {
+            Toast.makeText(this, "Error saving settings", Toast.LENGTH_SHORT).show();
+            e.printStackTrace();
+        }
+    }
+
+    public Switch getThemeSwitch() {
+        if (themeSwitch == null) {
+            themeSwitch = findViewById(R.id.theme_switch);
+        }
+        return themeSwitch;
     }
 }
+
--- app/src/main/java/edu/utsa/cs443/gpacalculator/controller/SettingsController.java
+++ app/src/main/java/edu/utsa/cs443/gpacalculator/controller/SettingsController.java
@@ -3,6 +3,7 @@ package edu.utsa.cs443.gpacalculator.controller;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Switch;
@@ -10,14 +11,20 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 import edu.utsa.cs443.gpacalculator.AboutActivity;
-import edu.utsa.cs443.gpacalculator.R;
 import edu.utsa.cs443.gpacalculator.HelpActivity;
+import edu.utsa.cs443.gpacalculator.R;
+import edu.utsa.cs443.gpacalculator.SettingsActivity;
 
 public class SettingsController implements View.OnClickListener {
 
     private final AppCompatActivity activity;
     private final Switch themeSwitch;
+    private final Switch notificationSwitch;
 
     public SettingsController(AppCompatActivity activity) {
         this.activity = activity;
@@ -26,15 +33,21 @@ public class SettingsController implements View.OnClickListener {
         Button aboutButton = activity.findViewById(R.id.about_button);
         Button helpButton = activity.findViewById(R.id.help_button);
         themeSwitch = activity.findViewById(R.id.theme_switch);
+        notificationSwitch = activity.findViewById(R.id.notification_checkbox);
+        Button saveButton = activity.findViewById(R.id.save_button);
 
         aboutButton.setOnClickListener(this);
         helpButton.setOnClickListener(this);
         themeSwitch.setOnClickListener(this);
+        notificationSwitch.setOnClickListener(this);
+        saveButton.setOnClickListener(this);
 
-        // Set the initial state of the theme switch
+        // Set the initial state of the switches
         SharedPreferences preferences = activity.getPreferences(Context.MODE_PRIVATE);
         boolean isDarkTheme = preferences.getBoolean("isDarkTheme", false);
+        boolean isNotificationEnabled = preferences.getBoolean("isNotificationEnabled", true);
         themeSwitch.setChecked(isDarkTheme);
+        notificationSwitch.setChecked(isNotificationEnabled);
         if (isDarkTheme) {
             activity.setTheme(R.style.DarkTheme_GpaCalculator);
         }
@@ -52,9 +65,14 @@ public class SettingsController implements View.OnClickListener {
             case R.id.theme_switch:
                 toggleTheme();
                 break;
+            case R.id.notification_checkbox:
+                toggleNotification();
+                break;
+            case R.id.save_button:
+                saveSettings();
+                break;
         }
     }
-
 
     private void showAboutScreen() {
         Intent intent = new Intent(activity, AboutActivity.class);
@@ -87,5 +105,51 @@ public class SettingsController implements View.OnClickListener {
         Intent intent = activity.getIntent();
         activity.finish();
         activity.startActivity(intent);
+    }
+
+    private void toggleNotification() {
+        // Get the current notification setting from shared preferences
+        SharedPreferences preferences = activity.getPreferences(Context.MODE_PRIVATE);
+        boolean isNotificationEnabled = preferences.getBoolean("isNotificationEnabled", true);
+
+        // Set the opposite notification setting in shared preferences
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putBoolean("isNotificationEnabled", !isNotificationEnabled);
+        editor.apply();
+
+        // Show  a toast to inform the user of the new notification setting
+        if (!isNotificationEnabled) {
+            Toast.makeText(activity, "Notifications enabled", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(activity, "Notifications disabled", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void saveSettings() {
+        // Get the current settings from the switches
+        boolean isDarkTheme = themeSwitch.isChecked();
+        boolean isNotificationEnabled = notificationSwitch.isChecked();
+
+        // Save the settings to shared preferences
+        SharedPreferences preferences = activity.getPreferences(Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putBoolean("isDarkTheme", isDarkTheme);
+        editor.putBoolean("isNotificationEnabled", isNotificationEnabled);
+        editor.apply();
+
+        // Save the settings to a file
+        try {
+            FileOutputStream outputStream = activity.openFileOutput("settings.txt", Context.MODE_PRIVATE);
+            outputStream.write(("Dark Theme: " + isDarkTheme + "\n").getBytes());
+            outputStream.write(("Notifications: " + isNotificationEnabled + "\n").getBytes());
+            outputStream.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // Show a toast to inform the user that the settings have been saved
+        Toast.makeText(activity, "Settings saved", Toast.LENGTH_SHORT).show();
     }
 }
