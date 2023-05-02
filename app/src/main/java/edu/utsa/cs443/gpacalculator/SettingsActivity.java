c983b1c69310ddab617531cbf3c90961bbdbb7a8
Esteban Leal <106554825+estebanleal1315@users.noreply.github.com>
2023-05-02 18:52:23 -0500

    Settings read file updates, might work

--- app/src/main/java/edu/utsa/cs443/gpacalculator/SettingsActivity.java
+++ app/src/main/java/edu/utsa/cs443/gpacalculator/SettingsActivity.java
@@ -18,8 +18,11 @@ import android.widget.CheckBox;
 import android.widget.Switch;
 import android.widget.Toast;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 
 import edu.utsa.cs443.gpacalculator.R;
@@ -38,6 +41,11 @@ public class SettingsActivity extends AppCompatActivity {
      *     recently supplied in {@link #onSaveInstanceState}.  <b><i>Note: Otherwise it is null.</i></b>
      *
      */
+    /**
+     * Reads the settings from the file and updates the UI accordingly
+     */
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -53,21 +61,71 @@ public class SettingsActivity extends AppCompatActivity {
                 saveSettingsToFile();
             }
         });
+
+        // Load the settings from the file and update the UI
+        readSettingsFromFile();
     }
+
+
+    /**
+     * Reads the settings from the file and updates the UI accordingly
+     */
+
 
     /**
      * On click of save button press, current settings are written to file
      */
+
+    private void readSettingsFromFile() {
+        try {
+            FileInputStream fileInputStream = openFileInput("settings.txt");
+            InputStreamReader inputStreamReader = new InputStreamReader(fileInputStream);
+            BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+
+            // Read the settings from the file and split the lines
+            String settingsString = bufferedReader.readLine();
+
+            // Get the notification checkbox and theme switch views
+            CheckBox notificationCheckbox = findViewById(R.id.notification_checkbox);
+            themeSwitch = findViewById(R.id.theme_switch); // update themeSwitch variable
+
+            // Update the notification checkbox based on the settings
+            if (settingsString.contains("Notifications: On")) {
+                notificationCheckbox.setChecked(true);
+            } else {
+                notificationCheckbox.setChecked(false);
+            }
+
+            // Update the theme switch based on the settings
+            if (settingsString.contains("Dark Theme: On")) {
+                themeSwitch.setChecked(true);
+            } else {
+                themeSwitch.setChecked(false);
+            }
+
+            bufferedReader.close();
+            Toast.makeText(this, "Settings loaded", Toast.LENGTH_SHORT).show();
+        } catch (IOException e) {
+            Toast.makeText(this, "Error loading settings", Toast.LENGTH_SHORT).show();
+            e.printStackTrace();
+        }
+    }
+
+
     private void saveSettingsToFile() {
         // Get the current state of the notification checkbox and theme switch
         boolean notificationsEnabled = ((CheckBox) findViewById(R.id.notification_checkbox)).isChecked();
         boolean isDarkTheme = ((Switch) findViewById(R.id.theme_switch)).isChecked();
 
+        // Create a string that represents the current settings
+        String settingsString = "Notifications: " + (notificationsEnabled ? "On" : "Off") + "\n"
+                + "Dark Theme: " + (isDarkTheme ? "On" : "Off");
+
         // Write the settings to a file
         try {
             FileOutputStream fileOutputStream = openFileOutput("settings.txt", MODE_PRIVATE);
             OutputStreamWriter outputStreamWriter = new OutputStreamWriter(fileOutputStream);
-            outputStreamWriter.write(notificationsEnabled + "," + isDarkTheme);
+            outputStreamWriter.write(settingsString);
             outputStreamWriter.close();
             Toast.makeText(this, "Settings saved", Toast.LENGTH_SHORT).show();
         } catch (IOException e) {
